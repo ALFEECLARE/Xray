19fb597248719bd2d1041d33c5b187eded687b19
ALFEECLARE <Main_User@192.168.12.10>
2026-09-10 16:24:06 +0900

    共通ライブラリの反映漏れ

--- src/main/java/fr/atesab/xray/utils/RenderUtils.java
+++ src/main/java/fr/atesab/xray/utils/RenderUtils.java
@@ -3,6 +3,7 @@ package fr.atesab.xray.utils;
 import java.util.Map;
 
 import org.joml.Matrix4f;
+import org.joml.Vector3f;
 
 import com.mojang.blaze3d.pipeline.BlendFunction;
 import com.mojang.blaze3d.pipeline.ColorTargetState;
@@ -16,12 +17,16 @@ import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexFormat;
 
+import net.minecraft.client.Camera;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.client.renderer.rendertype.LayeringTransform;
 import net.minecraft.client.renderer.rendertype.OutputTarget;
 import net.minecraft.client.renderer.rendertype.RenderSetup;
 import net.minecraft.client.renderer.rendertype.RenderSetup.RenderSetupBuilder;
 import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.util.Mth;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
@@ -31,7 +36,6 @@ public class RenderUtils {
 		public static final String SHADER_POSITION_TEX_COLOR = "core/position_tex_color";
 		public static final String SHADER_LINES = "core/rendertype_lines";
 
-	    
 		public static enum Shaders {
 			POSITION_TEX(SHADER_POSITION_TEX),
 			POSITION_COLOR(SHADER_POSITION_COLOR),
@@ -55,7 +59,6 @@ public class RenderUtils {
                         float x2, float y2,
                         float z2, float r, float g, float b, float a, float normalX, float normalY, float normalZ, float lineWidth) {
                 Matrix4f pose = stack.last().pose();
-                //PoseStack.Pose matrix3f = stack.last();
                 builder.addVertex(pose, x1, y1, z1).setColor(r, g, b, a).setLineWidth(lineWidth)
                                 .setNormal(normalX, normalY, normalZ);
                 builder.addVertex(pose, x2, y2, z2).setColor(r, g, b, a).setLineWidth(lineWidth)
@@ -74,7 +77,6 @@ public class RenderUtils {
 
         public static void renderLineBoxVanillaStyle(PoseStack stack, RenderingChannelBuilder builder, double minX, double minY, double minZ, double maxX, double maxY, double maxZ, float red1, float green1, float blue1, float alpha, float shadowRed, float shadowGreen, float shadowBlue, float lineWidth) {
             Matrix4f pose = stack.last().pose();
-            //PoseStack.Pose matrix3f = poseStack.last();
             float f = (float)minX;
             float f1 = (float)minY;
             float f2 = (float)minZ;
@@ -110,29 +112,48 @@ public class RenderUtils {
         public static RenderType getLineRenderType(String label) {
         	return buildRenderType(
 	        			label, 
-	        			buildPipeline("pipeline/lines", RenderPipelines.LINES_SNIPPET, RenderUtils.Shaders.LINES, BlendFunction.TRANSLUCENT, CompareOp.ALWAYS_PASS, DefaultVertexFormat.POSITION_COLOR_NORMAL_LINE_WIDTH, VertexFormat.Mode.LINES, false, null),
-	        			LayeringTransform.NO_LAYERING,
+	        			buildPipeline("pipeline/lines", RenderPipelines.LINES_SNIPPET, RenderUtils.Shaders.LINES, BlendFunction.TRANSLUCENT, CompareOp.ALWAYS_PASS, DefaultVertexFormat.POSITION_COLOR_LINE_WIDTH, VertexFormat.Mode.LINES, false, null, null),
+	        			LayeringTransform.VIEW_OFFSET_Z_LAYERING_FORWARD,
 	        			OutputTarget.ITEM_ENTITY_TARGET
 	        			);
         }
 
-        public static RenderPipeline buildEntityPipeline(String location) {
-        	return buildPipeline(location, RenderPipelines.MATRICES_FOG_SNIPPET, RenderUtils.Shaders.POSITION_COLOR, new BlendFunction(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA, SourceFactor.ONE, DestFactor.ZERO), CompareOp.ALWAYS_PASS, DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.QUADS, null, null);
+        public static RenderType buildEntityRenderType(String label) {
+        	return buildRenderType(
+	        			label, 
+	        			buildPipeline("pipeline/entity_solid", RenderPipelines.ENTITY_SNIPPET, RenderUtils.Shaders.POSITION_COLOR, new BlendFunction(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA, SourceFactor.ONE, DestFactor.ZERO), CompareOp.ALWAYS_PASS, DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.QUADS, false, null,null),
+	        			LayeringTransform.VIEW_OFFSET_Z_LAYERING_FORWARD,
+	        			OutputTarget.ITEM_ENTITY_TARGET
+	        			);
         }
         
-        public static RenderPipeline buildGuiPipeline(String location) {
-        	return buildPipeline(location, RenderPipelines.GUI_TEXTURED_SNIPPET, RenderUtils.Shaders.POSITION_TEX, BlendFunction.TRANSLUCENT, CompareOp.ALWAYS_PASS, DefaultVertexFormat.POSITION_TEX, VertexFormat.Mode.QUADS, null, null);
+        public static RenderType buildGuiRenderType(String label) {
+        	return buildRenderType(
+    				label,
+    				buildPipeline("pipeline/gui_textured", RenderPipelines.GUI_TEXTURED_SNIPPET, RenderUtils.Shaders.POSITION_TEX, BlendFunction.TRANSLUCENT, CompareOp.ALWAYS_PASS, DefaultVertexFormat.POSITION_TEX, VertexFormat.Mode.QUADS, null, null, null),
+    				null,
+    				null
+    				);
         }
-        
+
         public static RenderType getColoredGuiRenderType(String label) {
         	return buildRenderType(
         				label,
-        				buildPipeline("pipeline/gui_textured", RenderPipelines.GUI_TEXTURED_SNIPPET, RenderUtils.Shaders.POSITION_TEX_COLOR, BlendFunction.TRANSLUCENT, CompareOp.ALWAYS_PASS, DefaultVertexFormat.POSITION_TEX_COLOR, VertexFormat.Mode.QUADS, null, null),
+        				buildPipeline("pipeline/gui_textured", RenderPipelines.GUI_TEXTURED_SNIPPET, RenderUtils.Shaders.POSITION_TEX_COLOR, BlendFunction.TRANSLUCENT, CompareOp.ALWAYS_PASS, DefaultVertexFormat.POSITION_TEX_COLOR, VertexFormat.Mode.QUADS, false, null, "Sampler1"),
         				null,
         				null
         				);
         }
         
+        public static RenderType getTextParticleRenderType(String label) {
+        	return buildRenderType(
+        				label,
+        				buildPipeline("pipeline/opaque_particle", RenderPipelines.PARTICLE_SNIPPET, RenderUtils.Shaders.POSITION_TEX, BlendFunction.OVERLAY, CompareOp.ALWAYS_PASS, DefaultVertexFormat.POSITION_TEX, VertexFormat.Mode.QUADS, false, null, null),
+        				LayeringTransform.VIEW_OFFSET_Z_LAYERING_FORWARD,
+        				OutputTarget.ITEM_ENTITY_TARGET
+        				);
+        }
+
         public static RenderType buildRenderType(String label, RenderPipeline pipeline, LayeringTransform layerTransform, OutputTarget outputTarget) {
         	RenderSetupBuilder builder = RenderSetup.builder(pipeline);
         	if (layerTransform != null) {
@@ -144,7 +165,7 @@ public class RenderUtils {
         	return RenderType.create(label,builder.createRenderSetup());       
         }
         
-        public static RenderPipeline buildPipeline(String location, RenderPipeline.Snippet base, RenderUtils.Shaders shader, BlendFunction blendFunction, CompareOp depthFunction, VertexFormat defaultFormat, VertexFormat.Mode defaultmode, Boolean isCull, Map<String, UniformType> uniformMap) {
+        public static RenderPipeline buildPipeline(String location, RenderPipeline.Snippet base, RenderUtils.Shaders shader, BlendFunction blendFunction, CompareOp depthFunction, VertexFormat defaultFormat, VertexFormat.Mode defaultmode, Boolean isCull, Map<String, UniformType> uniformMap, String samplerName) {
         	RenderPipeline.Builder builder = RenderPipeline.builder(base);
         		builder.withLocation(location);
         	if (shader != null) {
@@ -164,77 +185,59 @@ public class RenderUtils {
         	if (isCull != null) {
         		builder.withCull(isCull);
         	}
+        	if (samplerName != null) {
+        		builder.withSampler(samplerName);
+        	}
     		return builder.withVertexFormat(defaultFormat, defaultmode).build();
         }
         
-        /*
-        public static RenderSystem.AutoStorageIndexBuffer buildBuffer(VertexFormat.Mode mode) {
-        	return RenderSystem.getSequentialBuffer(mode);
-        }
-        */
-
-        /*
-        public static GpuTexture getGpuTexture(Identifier location, TextureFormat format) {
-        	return RenderSystem.getDevice().createTexture(location.getNamespace(),GpuTexture.USAGE_COPY_SRC, format, 0, 0, 0 , 0);
-        }
-        public static void renderIfExists(RenderPipeline usingPipeLine, RenderSystem.AutoStorageIndexBuffer autoStorageBuffer) {
-        	renderIfExists(usingPipeLine, autoStorageBuffer, 72);
-        }
-       
-        public static void renderIfExists(RenderPipeline usingPipeLine, RenderSystem.AutoStorageIndexBuffer autoStorageBuffer, int bufferSize) {
-            GpuBuffer gpuBuffer = RenderSystem.getDevice()
-                    .createBuffer(() -> "vertext simple buffer", GpuBuffer.USAGE_MAP_WRITE, bufferSize);
-    		RenderTarget renderTarget = Minecraft.getInstance().getMainRenderTarget();
-            try (RenderPass renderpass = RenderSystem.getDevice()
-                    .createCommandEncoder()
-                    .createRenderPass(() -> "vertext simple RenderPass", renderTarget.getColorTextureView(), OptionalInt.empty(), renderTarget.getDepthTextureView(), OptionalDouble.empty())) {
-                renderpass.setPipeline(usingPipeLine);
-                renderpass.setIndexBuffer(autoStorageBuffer.getBuffer(36), autoStorageBuffer.type());
-                renderpass.setVertexBuffer(0, gpuBuffer);
-                renderpass.drawIndexed(0, 0, 0, 0);
-            }
-        }
-        	
-        public static void renderIfExists(BufferBuilder buffer, RenderPipeline usingPipeLine, RenderSystem.AutoStorageIndexBuffer autoStorageBuffer) {
-    		MeshData mesh = buffer.build();
-    		if (mesh != null) {
-    			//GpuBuffer vertexBuffer = RenderSystem.getDevice()
-    			//		.createBuffer(() -> "vertex buffer", GpuBuffer.USAGE_MAP_WRITE, mesh.vertexBuffer());
-    			GpuBuffer vertexBuffer =
-    					usingPipeLine.getVertexFormat().uploadImmediateVertexBuffer(mesh.vertexBuffer());
-
-    			RenderTarget renderTarget = Minecraft.getInstance().getMainRenderTarget();
-    			GpuTextureView texture = renderTarget.getColorTextureView();
-    			int drawStateCount = mesh.drawState().indexCount();
-    			if (texture == null || drawStateCount == 0) {
-    				return;
-    			}
-
-				Matrix4fStack matrix4fStack = RenderSystem.getModelViewStack();
-				matrix4fStack.pushMatrix();
-
-    			GpuBuffer gpuBuffer = autoStorageBuffer.getBuffer(drawStateCount);
-    			try (RenderPass renderPass = RenderSystem.getDevice().createCommandEncoder()
-    					.createRenderPass(() -> "vertex rendePass", texture, OptionalInt.empty(), renderTarget.getDepthTextureView(), OptionalDouble.empty())) {
-
-    				renderPass.setPipeline(usingPipeLine);
-    				RenderSystem.bindDefaultUniforms(renderPass);
-    				renderPass.setIndexBuffer(gpuBuffer, autoStorageBuffer.type());
-    				renderPass.setVertexBuffer(0, vertexBuffer);
-    				renderPass.drawIndexed(0, 0, drawStateCount, 0);
-
-    			}
-    			
-				matrix4fStack.popMatrix();
-    			mesh.close();
-    		}
-        }
-         */
-
         public static float getScaleValue(int sourceValue, int targetValue) {
         	return ((float)(Math.ceil(targetValue * 10 / sourceValue)) / 10);
         }
         
-        private RenderUtils() {
+        public static GuiGraphicsExtractor createGuiGraphicsExtractor() {
+        	Minecraft mc = Minecraft.getInstance();
+            int xMouse = (int)mc.mouseHandler.getScaledXPos(mc.getWindow());
+            int yMouse = (int)mc.mouseHandler.getScaledYPos(mc.getWindow());
+            return new GuiGraphicsExtractor(mc, mc.gameRenderer.getGameRenderState().guiRenderState, xMouse, yMouse);
+        }
+    	
+    	public static Vector3f worldSpaceToScreenSpace(float x, float y, float z) {
+    		Minecraft mc = Minecraft.getInstance();
+    		Camera camera = mc.gameRenderer.getMainCamera();
+    		double PI = 3.14159;
+
+    		Vec3 camPos = camera.position();
+    		double pX = x - camPos.x();
+    		double pY = y - camPos.y();
+    		double pZ = z - camPos.z();
+    		double opX = pX;
+    		double opY = pY;
+    		double opZ = pZ;
+    		double yawRad = Mth.wrapDegrees(camera.yRot()) * PI / 180;
+    		double pitRad = Mth.wrapDegrees(camera.xRot()) * PI / 180;
+    		double distance = Math.sqrt(Math.pow(pX,2) + Math.pow(pY,2) + Math.pow(pZ,2));
+
+    		pZ = opZ * Math.cos(yawRad) - opX * Math.sin(yawRad); 
+    		pX = opX * Math.cos(yawRad) + opZ * Math.sin(yawRad);
+    		
+    		opX = pX;
+    		opY = pY;
+    		opZ = pZ;
+
+    		pZ = opZ * Math.cos(pitRad) - opY * Math.sin(pitRad);
+    		pY = opY * Math.cos(pitRad) + opZ * Math.sin(pitRad);
+
+		    int screenWidth = mc.getWindow().getGuiScaledWidth();
+		    int screenHeight = mc.getWindow().getGuiScaledHeight();
+		    int fov = mc.options.fov().get();
+		    double screenDist = screenWidth / 3.5 / Math.tan(fov / 2 * PI / 180);
+		    
+		    double screenX = -pX / pZ * screenDist + screenWidth / 2;
+    		double screenY = -pY / pZ * screenDist + screenHeight / 2;
+    		return new Vector3f((float)screenX, (float)screenY, (float)(distance * (pZ / Math.abs(pZ))));
+    	}
+
+		private RenderUtils() {
         }
 }
